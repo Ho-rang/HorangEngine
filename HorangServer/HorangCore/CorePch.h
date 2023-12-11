79f02231f4a9403ab82a193af01033a4d7218a9c
성원 <clover24802@gmail.com>
2023-12-11 16:13:19 +0900

    Project Setting

---
+++ HorangServer/HorangCore/CorePch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/// <summary>
+/// 외부에서 참조할 파일들
+/// </summary>
+
+#include "Types.h"
+
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#include <iostream>
